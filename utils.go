81f50fb07d9d080842ad6b680a8c6bcfe13afe16
Leonid Shangin <leonid.shangin@math.msu.ru>
2026-06-13 12:31:51 +0300

    Add docs

--- utils.go
+++ utils.go
@@ -4,6 +4,7 @@ import "unsafe"
 
 type void struct{}
 
+// AsReadOnly converts a slice of bidirectional channels to read only ones.
 func AsReadOnly[T any](chs []chan T) []<-chan T {
 	outpChs := make([]<-chan T, len(chs))
 	for i := range outpChs {
@@ -13,10 +14,20 @@ func AsReadOnly[T any](chs []chan T) []<-chan T {
 	return outpChs
 }
 
+// AsReadOnlyWithUnsafe converts a slice of bidirectional channels to read only ones.
 func AsReadOnlyWithUnsafe[T any](chs []chan T) []<-chan T {
 	return *(*[]<-chan T)(unsafe.Pointer(&chs))
 }
 
+// MakeSliceOfChs makes a slice of bidirectional channels and initializes each of them.
+//
+// Params:
+//   - 'nChs' - amount of channels
+//   - 'bufSz' - buffer size (use '0' if no buffer need)
+//
+// Returns:
+//   - slice of initialized bidirectional channels
+//   - function of closing all returned channels
 func MakeSliceOfChs[T any](nChs int, bufSz int) (_ []chan T, closeFunc func()) {
 
 	if nChs <= 0 {
